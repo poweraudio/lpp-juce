fd3601e356267eb59fe7ecb84eef830ad2a1fd3c
KottV <kill_it@mail.ru>
2020-07-20 21:30:11 +1000

    lineup

--- modules/juce_audio_processors/processors/juce_AudioProcessor.h
+++ modules/juce_audio_processors/processors/juce_AudioProcessor.h
@@ -1210,8 +1210,8 @@ public:
         wrapperType_RTAS,
         wrapperType_AAX,
         wrapperType_Standalone,
-        wrapperType_Unity
-        , wrapperType_LV2
+        wrapperType_Unity,
+        wrapperType_LV2
     };
 
     /** When loaded by a plugin wrapper, this flag will be set to indicate the type
